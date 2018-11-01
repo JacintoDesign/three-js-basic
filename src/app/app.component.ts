3b6bba5063272b7bfee1ae79ab81c36b6c408c78
Jacinto Wong <contact@jacinto.design>
2018-11-01 18:05:46 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+"use strict";
+
 import { Component } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +11,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'jacinto-design-threejs-basic';
 }
+
+var renderer = new THREE.WebGLRenderer({canvas: document.getElementById('myCanvas'), antialias: true});
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.2, 3000);
+
+var scene = new THREE.Scene();
+
+var light = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(light);
+
+var light1 = new THREE.PointLight(0xffffff, 0.5);
+scene.add(light1);
+
+var geometry = new THREE.TorusKnotGeometry(100, 30, 100, 100);
+var material = new THREE.MeshLambertMaterial({color: 0xF33333});
+var mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, -2000);
+
+scene.add(mesh);
+
+requestAnimationFrame(render);
+
+function render() {
+  mesh.rotation.x += 0.05;
+  mesh.rotation.y += 0.01;
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+renderer.render(scene, camera);
